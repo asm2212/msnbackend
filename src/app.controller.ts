8b0cf7e4b13f71674ec246b0084d4ed798fc1a4d
asm2212 <asmareadmasu0@gmail.com>
2025-03-29 12:16:46 +0300

    Add msnbackend dependency, update routes, and modify greeting response

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('ff')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('gg')
   getHello(): string {
     return this.appService.getHello();
   }
