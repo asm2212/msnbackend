5652c8fed4d4a9f9a7c7d96fa243728228c36d79
asm2212 <asmareadmasu0@gmail.com>
2025-03-31 04:33:59 +0300

    Add DTOs for user, order, category, product, and review; implement role-based authorization and authentication guard

--- db/data-source.ts
+++ db/data-source.ts
@@ -8,12 +8,12 @@ export const dataSourceOptions: DataSourceOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: [],
-  migrations: [],
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/db/migrations/*{.ts,.js}'],
   logging: false,
   synchronize: false,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
-
+dataSource.initialize();
 export default dataSource;
